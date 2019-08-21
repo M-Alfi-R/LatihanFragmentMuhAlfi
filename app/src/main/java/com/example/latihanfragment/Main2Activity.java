63a8c42206e750f4d35ad7d7f2252ff77612f5d7
Muhamad Alfi <alvirizqi0@gmail.com>
2019-08-21 16:19:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanfragment/Main2Activity.java
@@ -0,0 +1,60 @@
+package com.example.latihanfragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        loadFragment(new Home());
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()){
+            case R.id.navigation_home:
+                fragment = new Home();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new Dashboard();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new Notification();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
